8a605b83ba19e7f73c425f0c8546598dd4a58d41
tonicostmarco <marcoricardo61b@gmail.com>
2026-02-12 21:03:31 -0300

    uri2621 JQPL and SQL

--- uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,9 +1,28 @@
 package com.devsuperior.uri2621.repositories;
 
+import com.devsuperior.uri2621.dtos.ProductMinDTO;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2621.entities.Product;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT products.name "
+            + "FROM products "
+            + "INNER JOIN providers ON products.id_providers = providers.id "
+            + "WHERE products.amount BETWEEN :min AND :max "
+            + "AND providers.name LIKE CONCAT(:beginName, '%')")
+    List<ProductMinProjection> search1(Integer min, Integer max, String beginName);
+
+
+    @Query("SELECT new com.devsuperior.uri2621.dtos.ProductMinDTO(obj.name) "
+            + "FROM Product obj "
+            + "WHERE obj.amount BETWEEN :min AND :max "
+            + "AND obj.provider.name LIKE CONCAT(:beginName, '%')")
+    List<ProductMinDTO> search2(Integer min, Integer max, String beginName);
 
 }
